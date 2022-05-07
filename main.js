3b5f609b5e5051bad353bbd36c236b6b855849bf
dlmobs <donia.l.mobli45@gmail.com>
2022-05-07 00:29:27 -0500

    added some comments

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const walletPK = new ethers.Wallet(prcoess.env.ADDRESS_PK, provider)
 const contract = new ethers.Contract(contractAddress, contractABI, walletPK)
 const usdcContract = new ethers.Contract(usdcAddress, erc20ABI, provider)
 
+// receivers are addresses where money is sent to; tokenAddresses are usdc and ftm
 const receivers = ["0xFBbe90DD04250B5E789Ed9d1bFBA2476E524a852", "0x8c128f336B479b142429a5f351Af225457a987Fa"]
 const tokenAddresses = ["0x04068DA6C83AFCFA0e13ba15A6696662335D5B75", "0x0000000000000000000000000000000000000000"]
 
@@ -32,16 +33,17 @@ const tryTX = async (contract, provider, usdcContract, recipients, tokenAddresse
         if (token == "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75") {
             var sendAmount = usdc_sendAmount
         }
+
         for (const recipient in recipients) {
             let tx;
             try {
                 tx = await contract.sendFunds(token, recipient, sendAmount);
                 tx = await tx.wait();
                 if (tx.status === 1) {
-                    console.log('first transaction complete');
+                    console.log(token, 'transaction complete');
                 }
             } catch (error) {
-                console.log('first transaction error:', error)
+                console.log(token, 'transaction error:', error)
             }
         }
     }
