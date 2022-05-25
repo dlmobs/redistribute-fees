c594a14aa574f7a37ff45c197669bbfc45ac3d70
dlmobs <donia.l.mobli45@gmail.com>
2022-05-24 20:01:42 -0500

    run test function

--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,4 @@
-const { transpose } = require('mathjs')
-const { parse } = require('csv-parse');
-const fs = require('fs'); 
-const { finished } = require('stream/promises')
 
-const BigNumber = require('bignumber.js')
 
 // // Initialize the parser
 // const parser = parse({delimiter: ','}, function (err, holders) {
@@ -17,7 +12,60 @@ const BigNumber = require('bignumber.js')
 
 
 
+/*
+send usdc and ftm from treasury contract to wallet
 
+treasury contract: 0x8e0dfafd247236b9025d3648f195386cad5b2da1
+wallet address: 0x8c128f336B479b142429a5f351Af225457a987Fa
+*/
+
+require('dotenv').config();
+const ethers = require('ethers');
+const contractABI = require('../abis/treasury.json')
+const erc20ABI = require('../abis/ERC20.json')
+const allAddresses = require('../data/addresses.json')
+const { availableFees } = require('./amountAvailable')
+
+// all addresses needed
+const usdcAddress = allAddresses[250].tokens.usdc.address
+const ftmAddress = allAddresses[250].tokens.ftm.address
+const addresses = [ftmAddress, usdcAddress]
+const contractAddress = allAddresses[250].addresses.treasury.address
+
+const provider = new ethers.providers.JsonRpcProvider("https://rpc.ftm.tools/")
+
+// wallet with write permissions, treasury, and usdc ethers objects (wallet is the address with access to write treasury contract)
+const walletPK = new ethers.Wallet(process.env.TREASURY_PK, provider)
+const contract = new ethers.Contract(contractAddress, contractABI, walletPK)
+const usdcContract = new ethers.Contract(usdcAddress, erc20ABI, provider)
+
+// wallet to receive funds
+const recipient = allAddresses[250].addresses.feesRecipient.address
+
+const collectFees = async (contract, addresses, recipient, provider) => {
+    // amount of usdc and ftm available
+    const sendAmount = [1000000, 1000000]
+
+    // try transactions, sends usdc and ftm to recipient address
+    for (const i in sendingAmounts) {
+        const sendAmount = sendingAmounts[i]
+        const token = addresses[i]
+
+        let tx;
+        try {
+            tx = await contract.sendFunds(token, recipient, sendAmount, {gasPrice: ethers.utils.parseUnits("1000", 'gwei'), gasLimit: 100000})
+            tx = await tx.wait();
+            if (tx.status === 1) {
+                console.log(token, 'transaction complete');
+            }
+        } catch (error) {
+            console.log(token, 'transaction error:', error)
+        }
+    }
+};
+
+// run transaction
+const run = collectFees(contract, addresses, recipient, provider)
 
 
 // const test = async () => {
@@ -38,11 +86,6 @@ const BigNumber = require('bignumber.js')
 // test().then((result) => {
 //     console.log(result)
 // })
-
-const a = 10000000000000000000000000
-const b = String(a)
-console.log(a)
-console.log(b)
 
 // console.log(fs.createReadStream("data/crosschain-tokenholders.csv").pipe(parse({delimiter: ','})))
 
